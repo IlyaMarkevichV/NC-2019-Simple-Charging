798ce935240d1e98efbd3c48c57401df5d812e87
Ильюха <markevich.ilya18@gmail.com>
2019-03-18 20:57:35 +0300

    front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { StartPageComponent } from './start-page/start-page.component';
+import { RouterModule, Routes } from "@angular/router";
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const appRoutes: Routes = [
+  {path: '', component:StartPageComponent},
+  {path: 'login', component:LoginPageComponent},
+  {path: 'signin', component:LoginPageComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
